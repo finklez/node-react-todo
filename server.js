ad00cd40a408df991c509c4b0e53c879cfa24493
eran.finkle@worthy.com <eran.finkle@worthy.com>
2018-11-19 22:05:07 +0200

    add Heroku support

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+// API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
@@ -16,5 +17,12 @@ app.post('/api/world', (req, res) => {
       `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
-
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 app.listen(port, () => console.log(`Listening on port ${port}`));
